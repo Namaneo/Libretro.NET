4b9788ead3795a5b6ec7e85a39f89f658e90c377
Julien Loir <julien.loir@corp.ovh.com>
2021-09-24 17:44:50 +0200

    Initial support for autogen using CppSharp

---
+++ Libretro.NET.Generator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace Libretro.NET.Generator
+{
+    public class LibretroLibrary : ILibrary
+    {
+        public override void Setup(Driver driver)
+        {
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+            options.Modules.Clear();
+            var module = options.AddModule("Libretro");
+            module.IncludeDirs.Add(".");
+            module.Headers.Add("libretro.h");
+        }
+
+        public override void SetupPasses(Driver driver)
+        {
+        }
+
+        public override void Preprocess(Driver driver, ASTContext ctx)
+        {
+        }
+
+        public override void Postprocess(Driver driver, ASTContext ctx)
+        {
+        }
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleDriver.Run(new LibretroLibrary());
+        }
+    }
+}
